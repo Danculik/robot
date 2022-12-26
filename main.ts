25e5e59e206f6700c387f762462197c55c8c5b0d
Danculik <121442990+Danculik@users.noreply.github.com>
2022-12-26 20:35:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    TurnRight()
+})
+input.onButtonPressed(Button.B, function () {
+    maqueen.motorStop(maqueen.Motors.All)
+})
 function TurnRight () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 237)
     basic.pause(500)
     maqueen.motorStop(maqueen.Motors.M1)
     basic.pause(1000)
 }
+let Red = 0
+let LedkaCislo = 0
 DFRobotMaqueenPlusV2.init()
-basic.showIcon(IconNames.Asleep)
+basic.showLeds(`
+    . . . . .
+    . . # . #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
 basic.forever(function () {
-    TurnRight()
+    LedkaCislo = randint(0, 3)
+    Red = randint(0, 16777216)
+    DFRobotMaqueenPlusV2.setIndexColor(LedkaCislo, Red)
+    basic.pause(500)
 })
