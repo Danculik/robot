3c807c9b6b55513d7e1c6dfd54e9793727fe5086
Danculik <121442990+Danculik@users.noreply.github.com>
2022-12-30 22:30:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,27 @@
 input.onButtonPressed(Button.A, function () {
     TurnRight()
 })
+IR.IR_callbackUser(function (message) {
+    if (message == 8) {
+        message = 0
+        maqueen.motorStop(maqueen.Motors.All)
+        DFRobotMaqueenPlusV2.showColor(0xff0000)
+    } else if (message == 37) {
+        message = 0
+        DFRobotMaqueenPlusV2.showColor(0x00ff00)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        basic.pause(1000)
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (message == 77) {
+        message = 0
+        turnleft()
+    } else if (message == 70) {
+        message = 0
+        TurnRight()
+    } else {
+        basic.showString("" + (message))
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     Blue = 0
     while (Blue == 0) {
@@ -31,21 +52,17 @@ function turnleft () {
     maqueen.motorStop(maqueen.Motors.M2)
     basic.pause(1000)
 }
-let Red = 0
-let LedkaCislo = 0
+let message = 0
 let distance = 0
 let Blue = 0
 DFRobotMaqueenPlusV2.init()
 basic.showLeds(`
     # . . . #
     . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
+    . . # . .
+    # # . # #
+    . # . # .
     `)
 basic.forever(function () {
-    LedkaCislo = randint(0, 3)
-    Red = randint(0, 16777216)
-    DFRobotMaqueenPlusV2.setIndexColor(LedkaCislo, Red)
-    basic.pause(500)
+    message = IR.IR_read()
 })
